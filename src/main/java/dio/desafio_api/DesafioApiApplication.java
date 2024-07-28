8c9dd642a1b7b93ceb97304fc2d7d5a9a6824c4e
promyr <jesusbelchior@outlook.com>
2024-07-28 18:51:40 -0300

    Initial commit

---
+++ src/main/java/dio/desafio_api/DesafioApiApplication.java
@@ -0,0 +1,13 @@
+package dio.desafio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafio_api/DesafioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
